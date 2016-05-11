83155112e6017d0e0498aca9bf6c36892f60aa46
kmcgowan0 <i7673473@Mac27779.local>
2016-05-11 10:43:17 +0100

    logo back on screen!!

--- aiplaytestfinal/ViewController.swift
+++ aiplaytestfinal/ViewController.swift
@@ -48,38 +48,29 @@ class ViewController: UIViewController {
     }
 
     func loadScreen(){
-        //before a video is selected
+        
         if UIScreen.screens().count > 1 {
-            //find the second screen (the 'as! UIScreen' is not needed in Xcode 7 and above)
+            
             let secondScreen = UIScreen.screens()[1]
             
-            //set up a window for the screen using the screens pixel dimensions
             secondWindow = UIWindow(frame: secondScreen.bounds)
             
-            //tell the window which screen to use
             secondWindow?.screen = secondScreen
             
-            //set the dimensions for the view for the external screen so it fills the screen
             secondScreenView = UIView(frame: secondWindow!.frame)
             
-            //add the view to the second screens window
             secondWindow?.addSubview(secondScreenView!)
             
-            //unhide the window
             secondWindow?.hidden = false
-            //
-            //            //customised the view
+         
             secondScreenView!.backgroundColor = UIColor.whiteColor()
-            //            //configure the label
+            
                         externalLabel.textAlignment = NSTextAlignment.Center
                         externalLabel.font = UIFont(name: "Helvetica", size: 50.0)
                         externalLabel.frame = secondScreenView!.bounds
                         externalLabel.text = ""
             externalLabel.backgroundColor = UIColor(patternImage: UIImage(named: "3182172")!)
-            
-
-            
-            //add the label to the view
+     
             secondScreenView!.addSubview(externalLabel)
         }
     }
@@ -104,7 +95,7 @@ class ViewController: UIViewController {
             //unhide the window
             secondWindow?.hidden = false
      
-//            //customised the view
+            //customised the view
             secondScreenView!.backgroundColor = UIColor.blackColor()
 
             
@@ -117,18 +108,13 @@ class ViewController: UIViewController {
             externalLabel.text = ""
             externalLabel.backgroundColor = UIColor(patternImage: UIImage(named: "3182172")!)
             
-            if videoPlayer.ready == false {
-                secondScreenView!.addSubview(videoPlayer)
-            }
-            videoPlayer.loadVideoID(videos[currentScreens].id)
-            
-            //add the label to the view
-            secondScreenView!.addSubview(videoPlayer)
-            
-            
+         
+            secondScreenView!.addSubview(externalLabel)
             
         }
     }
+    
+
     
     func registerForScreenNotifications(){
         let notificationCentre = NSNotificationCenter.defaultCenter()
@@ -136,6 +122,9 @@ class ViewController: UIViewController {
     }
     
     func playvideo() {
+        
+        secondScreenView!.addSubview(videoPlayer)
+        
         videoPlayer.loadVideoID(videos[currentScreens].id)
         if videoPlayer.ready {
             videoPlayer.play()
